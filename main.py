12167019915d1f5f35ee5536406d4f6ceff027a2
Sereno Dominguez <sereno@colostate.edu>
2023-10-02 00:10:44 +0900

    input validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.firefox.service import service
 from selenium.webdriver.firefox.service import Service
 
 import csv
+import re
 
 class Profile:
     def __init__(self, pname, fname, lname, pin, email, country):
@@ -25,18 +26,43 @@ def getProfiles() -> list:
     with open(filename, newline='') as csvfile:
         profilereader = csv.reader(csvfile)
         for row in profilereader:
+            if len(row) != 6:
+                raise ValueError("bad csv -- should be 6 fields in row: " + str(row))
             if row[0] == "pname":
                 continue
             for j in range(len(row)): 
                 row[j] = row[j].strip()
             profiles.append(Profile(row[0], row[1], row[2], row[3], row[4], row[5]))
     
-    # csv validation
+    # error checking
     profiles.sort(key=lambda x: x.pname)
     for i in range(len(profiles)):
+        # TODO: account for header row?
+        if profiles[i].pname == "profile name":
+            continue
+        #     profiles.remove(profiles[i])
+        # duplicate pnames
         if i < len(profiles)-1 and profiles[i].pname == profiles[i+1].pname:
             raise ValueError("bad csv -- duplicate profile name: " + profiles[i].pname)
-        if profiles[i].fname 
+        # fname/lname not just letters
+        justLetters = re.compile(r'^[a-zA-Z]+$')
+        if not justLetters.match(profiles[i].fname):
+            raise ValueError("bad csv -- should only be letters in first name: " + profiles[i].fname)
+        if not justLetters.match(profiles[i].lname):
+            raise ValueError("bad csv -- should only be letters in last name: " + profiles[i].lname)
+        # pin not 6-13 digits
+        sixToThirteenDigits = re.compile(r'^\d{6,13}$')
+        if not sixToThirteenDigits.match(profiles[i].pin):
+            raise ValueError("bad csv -- pin should be 6-13 digits: " + profiles[i].pin)
+        # email not valid
+        validEmail = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+        if not validEmail.match(profiles[i].email):
+            raise ValueError("bad csv -- email not valid: " + profiles[i].email)
+        # country code not valid
+        validCountryCode = re.compile(r'^[A-Z]{2}$')
+        if not validCountryCode.match(profiles[i].country):
+            raise ValueError("bad csv -- country code should be ISO 3166-1 alpha-2: "
+                             + profiles[i].country)
 
     return profiles
 
